54a5ac4eae135b528107e29651dd63c3dece4338
Taras <taras.zhilenko@gmail.com>
2024-05-11 14:13:43 -0400

    Android Logger, read file

--- tcp_client_app.py
+++ tcp_client_app.py
@@ -1,3 +1,4 @@
+import pathlib
 import socket
 import threading
 import time
@@ -5,11 +6,17 @@ import time
 from os import urandom
 
 from kivy.app import App
+from kivy.logger import Logger
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label  import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget  import Widget
+from kivy.utils import platform
+
+if platform == 'android':
+    from androidstorage4kivy import SharedStorage
+    from andrioid import api_version
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', True)
@@ -21,6 +28,10 @@ BUFF_SIZE = 16
 
 class MyCounterApp(App):
     def build(self):
+        Logger.info("lalala amznsdf3")
+        if platform == 'android':
+            Logger.info("api_version: " + str(api_version))
+        Logger.debug("lalala amznsdf4")
         self.data_dir = self.user_data_dir
         root_widget = self.build_root_widget()
         return root_widget
@@ -38,8 +49,11 @@ class MyCounterApp(App):
         return grid 
 
     def button_callback(self, obj):
+        Logger.info( "kivyyvik: button_push1")
+        Logger.debug("kivyyvik: button_push2")
         self.send_random_bytestring(obj)
         self.write_random_file(obj)
+        self.read_file(obj)
 
     def send_random_bytestring(self, obj):
         client = socket.socket()
@@ -50,8 +64,19 @@ class MyCounterApp(App):
         client.close()
 
     def write_random_file(self, obj):
+        pathlib.Path(self.data_dir).mkdir(parents=True, exist_ok=True)
         with open(f'{self.data_dir}/text.txt', 'w') as f:
             f.write('Hello world!')
+        if platform == 'android':
+            shared_path = SharedStorage().copy_to_shared(f'{self.data_dir}/text.txt')
+            self.info_label.text += '\n' + str(shared_path)
+            Logger.info( "kivyyvik_shared_storage1 " + str(shared_path))
+            Logger.info( "kivyyvik_shared_storage2 " + str(shared_path.getPath()))
+
+    def read_file(self, obj):
+        with open(f'{self.data_dir}/text.txt', 'r') as f:
+            line = f.readline().strip()
+            self.info_label.text += '\n' + line
 
 if __name__ == "__main__":
     MyCounterApp().run()
