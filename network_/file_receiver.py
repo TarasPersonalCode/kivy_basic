5bd7881a9d525b813d6328100b28301f56cbc335
Taras <taras.zhilenko@gmail.com>
2024-05-21 20:56:26 -0400

    add FileReceiver

---
+++ network_/file_receiver.py
@@ -0,0 +1,27 @@
+
+class NetworkFileReceiver:
+    def __init__(self, network_manager, filename, batch_size):
+        self.nm         = network_manager
+        self.filename   = filename
+        self.batch_size = batch_size
+        open(filename, 'wb').close()
+        self.filesize = None
+
+    def receive_batch(self):
+        if self.filesize is None:
+            self.filesize = self.nm.recv()
+        batch_ctr = self.batch_size
+        while self.filesize > 0 and batch_ctr > 0:
+            with open(self.filename, 'ab') as f:
+                chunk = self.nm.sock.recv(self.nm.buff_size)
+                f.write(chunk)
+                self.filesize -= len(chunk)
+                print(f'{self.filesize=}')
+            batch_ctr -= 1 
+        return self.filesize != 0
+
+    def receive_all(self):
+        if self.filesize is None:
+            self.receive_batch()
+        while self.filesize > 0:
+            self.receive_batch()
